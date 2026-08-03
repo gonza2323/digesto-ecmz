77518021b90df5471d7795616f144295fd9efe2e
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2026-08-03 16:36:17 -0300

    Setup Spring Boot and React projects

---
+++ backend/src/main/java/ar/edu/uncuyo/mzapata/digesto/DigestoApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.uncuyo.mzapata.digesto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigestoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ar/edu/uncuyo/mzapata/digesto/DigestoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.uncuyo.mzapata.digesto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
