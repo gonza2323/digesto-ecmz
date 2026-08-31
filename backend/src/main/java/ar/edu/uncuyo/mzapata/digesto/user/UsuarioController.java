858edc00ed7e1158c9a5f49e3976dd0ac366c06f
AgusOlivares <faolivares3112@gmail.com>
2026-08-31 13:49:23 -0300

    add update function

--- backend/src/main/java/ar/edu/uncuyo/mzapata/digesto/user/UsuarioController.java
+++ backend/src/main/java/ar/edu/uncuyo/mzapata/digesto/user/UsuarioController.java
@@ -1,10 +1,12 @@
 package ar.edu.uncuyo.mzapata.digesto.user;
 
+import ar.edu.uncuyo.mzapata.digesto.auth.CurrentUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     private final UsuarioService userService;
+
 
     @PostMapping("/crear")
     @PreAuthorize("hasRole('SUPERADMIN')")
@@ -30,22 +33,50 @@ public class UsuarioController {
     @PreAuthorize("hasRole('SUPERADMIN')")
     public ResponseEntity<Void> eliminarUsuario(@RequestBody UsuarioSummaryDto dto){
         try {
-            userService.deleteUsuario(dto);
+            userService.eliminarUsuario(dto);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    //TODO: Revisar como entra el formato del email a buscar. Verificar si esta vacio, incorrecto, malicioso, etc.
     @GetMapping("/buscar/{email}")
     @PreAuthorize("hasRole('SUPERADMIN')")
-    public ResponseEntity<UsuarioDetailDto> searchUsuario(@PathVariable String email) {
+    public ResponseEntity<UsuarioDetailDto> buscarUsuario(@PathVariable String email) {
         try {
-            return ResponseEntity.ok(userService.searchUsuario(email));
+            return ResponseEntity.ok(userService.buscarUsuario(email));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<UsuarioDetailDto> buscarMiUsuario(@AuthenticationPrincipal CurrentUser currentUser) {
+        try {
+            if (currentUser != null) {
+                String id = currentUser.getName();
+                return ResponseEntity.ok(userService.getUsuario(id));
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PutMapping("/actualizar")
+    public ResponseEntity<UsuarioDetailDto> actualizarUsuario(@AuthenticationPrincipal CurrentUser currentUser, @RequestBody UsuarioRequestDto dto){
+        try {
+            if (currentUser != null) {
+                String id = currentUser.getName();
+                return ResponseEntity.ok(userService.actualizarUsuario(id, dto));
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 
 }
--- backend/src/main/java/ar/edu/uncuyo/mzapata/digesto/user/UsuarioService.java
+++ backend/src/main/java/ar/edu/uncuyo/mzapata/digesto/user/UsuarioService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
@@ -22,11 +23,13 @@ public class UsuarioService {
             throw new RuntimeException("No se pudo crear el usuario");
         }
 
+
+
         Usuario usuario = Usuario.builder()
                 .firstname(dto.getFirstname())
                 .lastname(dto.getLastname())
                 .email(dto.getEmail())
-                .passwordHash("")
+                .passwordHash(dto.getFirstname().toLowerCase() + dto.getLastname().toLowerCase())
                 .mustChangePassword(true)
                 .role(dto.getRole())
                 .build();
@@ -36,7 +39,7 @@ public class UsuarioService {
     }
 
     @Transactional
-    public void deleteUsuario(UsuarioSummaryDto dto){
+    public void eliminarUsuario(UsuarioSummaryDto dto){
         Usuario usuarioActual = repository.findByEmailAndDeletedFalse(dto.getEmail())
                 .orElseThrow(() -> new RuntimeException("No se pudo encontrar al usuario"));
 
@@ -44,11 +47,47 @@ public class UsuarioService {
     }
 
     @Transactional
-    public UsuarioDetailDto searchUsuario(String email) {
+    public UsuarioDetailDto buscarUsuario(String email) {
         return repository.findByEmailAndDeletedFalse(email)
                 .map(this::toDetailDto)
                 .orElseThrow(() -> new BusinessException("No se pudo encontrar al usuario"));
     }
+
+    //funcion para traer los datos completos del usuario actual
+    @Transactional
+    public UsuarioDetailDto getUsuario(String id) {
+        return repository.findByIdAndDeletedFalse(UUID.fromString(id))
+                .map(this::toDetailDto)
+                .orElseThrow(() -> new BusinessException("No se pudo encontrar al usuario"));
+    }
+
+    @Transactional
+    //TODO: Mejorar mensaje de error
+    public UsuarioDetailDto actualizarUsuario(String id, UsuarioRequestDto newData){
+        Optional<Usuario> usuarioActual = repository.findByIdAndDeletedFalse(UUID.fromString(id));
+
+        if (!usuarioActual.isPresent()) {
+            throw new RuntimeException("Ha ocurrido un error");
+        }
+
+        if (usuarioActual.get().isMustChangePassword()){
+            throw new RuntimeException("Debe cambiar la primera contraseña"); //poner error mas claro
+        }
+
+        if (!validarUpdate(newData, UUID.fromString(id))){
+            throw new RuntimeException("Uno o mas campos invalidos");
+        }
+
+        usuarioActual.get().setFirstname(newData.getFirstname());
+        usuarioActual.get().setLastname(newData.getLastname());
+        usuarioActual.get().setEmail(newData.getEmail());
+        usuarioActual.get().setPasswordHash(newData.getPassword());
+
+        repository.save(usuarioActual.get());
+        return toDetailDto(usuarioActual.get());
+    }
+
+    // =========== Funciones De Utilidad ===========
 
     //Valido Campos
     //TODO: Mejorar la validacion del email y revisar
@@ -71,4 +110,16 @@ public class UsuarioService {
                 .role(usuario.getRole())
                 .build();
     }
+    //validacion de campos para el update
+    private boolean validarUpdate(UsuarioRequestDto dto, UUID id){
+        if (dto.getFirstname() == null || dto.getFirstname().isBlank()
+                || dto.getLastname() == null || dto.getLastname().isBlank()
+                || dto.getEmail() == null || dto.getEmail().isBlank()
+                || dto.getPassword() == null || dto.getPassword().isBlank() ) {
+            return false;
+        }
+        return !repository.existsByEmailAndIdNotAndDeletedFalse(dto.getEmail(), id);
+    }
+
+
 }
